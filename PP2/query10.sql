f85fd1f8957da523c01493f15ddd79999d147d8a
Suyan Qu <squ27@wisc.edu>
2019-03-04 17:24:35 -0600

    Update query 10 without using groupint sets

--- PP2/query10.sql
+++ PP2/query10.sql
@@ -2,14 +2,21 @@ with type_sale as (select type,
                           extract(year from weekdate)    as year,
                           extract(quarter from weekdate) as quarters,
                           sum(weeklysales)               as sum
-                   from sales
-                          join stores on sales.store = stores.store
-                   group by grouping sets ((type, year),(type, year, quarters)))
-select A.year, A.quarters, A.sum as A_sale, B.sum as B_sale, C.sum as C_sale
-from type_sale as A
-       join (type_sale as B join type_sale as C on B.year = C.year and (B.quarters = C.quarters or (B.quarters isnull and c.quarters isnull) ))
-         on A.year = B.year and (A.quarters = B.quarters or (A.quarters isnull and c.quarters isnull ))
-where A.type = 'A'
-  and B.type = 'B'
-  and C.type = 'C'
-order by year, quarters nulls last;
+                  from sales
+                  join stores on sales.store = stores.store
+                  group by type, year, quarters),
+
+  summary (year, quarter, A_sale, B_sale, C_sale) as( select A.year, A.quarters, A.sum, B.sum, C.sum
+                                                      from type_sale as A
+                                                      join (type_sale as B join type_sale as C on B.year = C.year and (B.quarters = C.quarters))
+                                                      on A.year = B.year and (A.quarters = B.quarters)
+                                                      where A.type = 'A'
+                                                      and B.type = 'B'
+                                                      and C.type = 'C')
+
+select * from summary
+union all
+select year, NULL, sum(A_sale), sum(B_sale), sum(C_sale)
+from summary
+group by year
+order by year, quarter nulls last;
