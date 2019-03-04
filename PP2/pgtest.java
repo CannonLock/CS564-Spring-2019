26ed8c7bbb58b6a5b0727071b24d189c1639de31
Unknown <jialuogao980513@gmail.com>
2019-03-04 05:02:42 -0600

    Update pgtest.java finished? haven't tested

--- PP2/pgtest.java
+++ PP2/pgtest.java
@@ -24,32 +24,50 @@ public class pgtest {
         conn.close();
     }
 
-    private static void loop() throws SQLException {
+    private static void loop() throws SQLException, NumberFormatException, InputMismatchException {
         int n, seed = 0;
         Set<Integer> selected = new HashSet<>();
 
         String query = promptUserQuery();
         int N = getCountFromQuery(query);
+        System.out.print("How many samples do you want: ");
         do {
-            System.out.print("How many samples do you want: ");
-            n = Math.min(N, in.nextInt());
+			String sampleInput = in.next().toLowerCase();
+			if (sampleInput.equals("quit"))
+                break;
+			
+            n = Math.min(N, Integer.parseInt(sampleInput));
+			if(n < 1) {
+				System.out.print("Sample size input must be greater than zero, please re-enter your input: ");
+				continue;
+			}
+			
             System.out.print("Please enter the seed for sampling: ");
             seed = in.nextInt();
+			
+			System.out.print("Please select output mode: (Enter table name(save in new table) / stdout(standard output))");
+			String newtName = in.next();
+			
             Integer[] sampleRowNum = sampleNumer(N, n, seed, selected);
             N -= n;
-            Collections.addAll(selected, sampleRowNum);
-
-            printResultSet(getResultSetFromQuery(query));
-            System.out.println(Arrays.toString(sampleRowNum));
-
+			Collections.addAll(selected, sampleRowNum);
+			
+			rowQuery = selectRowQuery(query, sampleRowNum);
+			if(newtName.equals("stdout")){
+				printResultSet(getResultSetFromQuery(rowQuery));
+				System.out.println(Arrays.toString(sampleRowNum));
+			}
+			else {
+				rowQuery = saveResultSetQuery(newtName, rowQuery);
+				getResultSetFromQuery(rowQuery);
+				System.out.println("Results are saved into table: " + newtName);
+			}
+			
             if (N <= 0) {
                 System.out.println("No more samples available.");
                 break;
             }
-            System.out.print("Enter anything to continue sampling or quit to exit: ");
-            if (in.next().toLowerCase().equals("quit"))
-                break;
-
+            System.out.print("Enter the numbers of the additional samples to continue or quit to exit: ");
         } while (true);
     }
 
@@ -59,10 +77,13 @@ public class pgtest {
         if (line.charAt(0) == 't') {
             System.out.print("Please enter table name: ");
             return getQueryFromTableName(in.nextLine());
-        } else {
+        } else if (line.charAt(0) == 'q'){
             System.out.print("Please enter your query: ");
             return in.nextLine();
-        }
+        } else {
+			System.out.print("Please re-enter a valid input!");
+			return promptUserQuery();
+		}
     }
 
     private static Integer getCountFromQuery(String query) throws SQLException {
@@ -81,6 +102,18 @@ public class pgtest {
         return "select * from " + tableName + ";";
     }
 
+	private static String selectRowQuery(String query, int[] sampleRowNum) {
+		String whereString = "where rownum = " + sampleRowNum[0];
+		for(int i = 1; i < sampleRowNum.length; i++) {
+			whereString += "or rownum = " + sampleRowNum[i];
+		}
+		return "select * from ( select Row_Number() as rownum, * from (" + query + ") s1 ) s2" + whereString + ";";
+	}
+	
+	private static String saveResultSetQuery(String newtName, String query) {
+		return "insert into " + newtName + " " + query;
+	}
+	
     private static Integer[] sampleNumer(int R, int n, int seed, Set<Integer> selected) {
         Integer[] ret = new Integer[n];
         int m = 0, t = 0, N = R + selected.size();
