177ee0083acfdbb5f61da716b690b11f9fe0eae0
Shawn Zhong <public@shawnzhong.com>
2019-03-04 19:47:07 -0600

    [PP2] format query 10

--- PP2/query10.sql
+++ PP2/query10.sql
@@ -2,19 +2,21 @@ with type_sale as (select type,
                           extract(year from weekdate)    as year,
                           extract(quarter from weekdate) as quarters,
                           sum(weeklysales)               as sum
-                  from sales
-                  join stores on sales.store = stores.store
-                  group by type, year, quarters),
+                   from sales
+                          join stores on sales.store = stores.store
+                   group by type, year, quarters),
 
-  summary (year, quarter, A_sale, B_sale, C_sale) as( select A.year, A.quarters, A.sum, B.sum, C.sum
-                                                      from type_sale as A
-                                                      join (type_sale as B join type_sale as C on B.year = C.year and (B.quarters = C.quarters))
-                                                      on A.year = B.year and (A.quarters = B.quarters)
-                                                      where A.type = 'A'
-                                                      and B.type = 'B'
-                                                      and C.type = 'C')
+     summary (year, quarter, A_sale, B_sale, C_sale) as (
+       select A.year, A.quarters, A.sum, B.sum, C.sum
+       from type_sale as A
+              join (type_sale as B join type_sale as C on B.year = C.year and (B.quarters = C.quarters))
+                   on A.year = B.year and (A.quarters = B.quarters)
+       where A.type = 'A'
+         and B.type = 'B'
+         and C.type = 'C')
 
-select * from summary
+select *
+from summary
 union all
 select year, NULL, sum(A_sale), sum(B_sale), sum(C_sale)
 from summary
